d0c679d801413487340809fa78d738ad242dd969
unknown <sssergio@ymail.com>
2023-03-27 14:22:01 -0300

    Conclusão das modificações para completar o desafio.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if(Suite is null) { CriarExcecao("Não há Suites Cadastradas"); }
+            
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                CriarExcecao("Número de Hóspedes maior que a capacidade da Suite");
             }
         }
 
@@ -35,26 +34,28 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            if(Hospedes is null) { CriarExcecao("Não há Hóspedes Cadastrados"); }
+
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            if(Suite is null) { CriarExcecao("Não há Suites Cadastradas"); }
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados > 9)
             {
-                valor = 0;
+                valor = valor - valor*10/100;
             }
 
             return valor;
+        }
+
+        private void CriarExcecao(string mensagem)
+        {
+            throw new Exception(mensagem);
         }
     }
 }
